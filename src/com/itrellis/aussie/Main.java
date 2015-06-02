92920fb1506ebd502d90241780f4285257b49a0b
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-06-01 21:26:13 -0700

    first commit for AussieVoting challenge

---
+++ src/com/itrellis/aussie/Main.java
@@ -0,0 +1,8 @@
+package com.itrellis.aussie;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ test/com/itrellis/aussie/AussieTest.java
@@ -0,0 +1,16 @@
+package com.itrellis.aussie;
+
+import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jaime on 6/1/15.
+ */
+public class AussieTest extends TestSuite {
+
+    @Test public void testsAreWorking() {
+        assertEquals(true, true);
+    }
+
+}

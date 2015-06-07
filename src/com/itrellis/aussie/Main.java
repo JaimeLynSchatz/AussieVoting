c2e40ef7436712e665af059d7e611663b0b216e1
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-06-07 13:57:43 -0700

    a few collections created, not there yet

--- src/com/itrellis/aussie/Main.java
+++ src/com/itrellis/aussie/Main.java
@@ -1,4 +1,8 @@
 package com.itrellis.aussie;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /*
  * Sample Input
  * 1
@@ -25,51 +29,57 @@ public class Main {
                 this.numberOfCandidates = numberOfCandidates;
             }
 
-            public String candidates[] = new String[this.numberOfCandidates];
+            // not sure yet, but fairly sure this isn't needed anymore
+            //
+//            public String candidates[] = new String[this.numberOfCandidates];
+//            public String[] getCandidates() { return candidates; }
+//            public void setCandidates(String[] candidates) { this.candidates = candidates; }
 
-            // need to find a way to set the number of rows more dynamically
-            int votes[][] = new int[5][this.numberOfCandidates];
-
-            public int[][] getVotes() {
-                return votes;
+            class Candidate {
+                public Candidate(String name) {
+                    String candidateName = name;
+                }
             }
 
-            public void setVotes(int[][] votes) {
-                this.votes = votes;
+            List<Candidate> Candidates = new ArrayList<>();
+
+            class Vote {
+                public Vote(int numberOfCandidates) {
+                    int[] voterChoices = new int[numberOfCandidates];
+                }
             }
 
-            public String[] getCandidates() {
+            // use a Collection/List to set the number of rows dynamically
+            List<Vote> votes = new ArrayList<>();
 
-                return candidates;
-            }
-
-            public void setCandidates(String[] candidates) {
-                this.candidates = candidates;
-            }
         }
 
         // read the first line of the file here to get the numberOfElections
         // manually setting now
         int numberOfElections = 2;
-        for (int i = 0; i < numberOfElections; i++)
 
-
+        for (int i = 0; i < numberOfElections; i++) {
             Election election = new Election(3);
 
-        int voteArray[][] = new int[5][3];
-        int i;
-        int j;
-        for (i = 0; i < 5; i++) {
-            for (j = 0; j < 3; j++) {
-                // now what, genius?
+            int voteArray[][] = new int[5][3];
+            int j;
+            int k;
+            for (j = 0; j < 5; j++) {
+                for (k = 0; k < 3; k++) {
+                    // now what, genius?
+                }
             }
-        }
 
-        String[] candidates = new String[]{"Elizabeth Warren", "Bob Dole", "Mickey Mouse"};
-        election.setCandidates(candidates);
-        System.out.println(election.getVotes());
-        for (i = 0; i < candidates.length; i++) {
-            System.out.println(candidates[i]);
+            Election myElection = new Election(1);
+
+            myElection.Candidates.add(new Election.Candidate("Bob Dole"));
+
+            String[] candidates = new String[]{"Elizabeth Warren", "Bob Dole", "Mickey Mouse"};
+            election.setCandidates(candidates);
+            System.out.println(election.getVotes());
+            for (i = 0; i < candidates.length; i++) {
+                System.out.println(candidates[i]);
+            }
         }
     }
 }
